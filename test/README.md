215dcf60da323cd8ec7f7a5b4064767a4765a8f4
Daniël <daniel.emmery@protonmail.com>
2020-07-13 21:50:39 +0200

    Testing improvements

---
+++ test/README.md
@@ -0,0 +1,3 @@
+## Unit testing
+
+Unit testing is currently mostly limited to validation testing. Generated results will be compared to known (literature) data and the test framework will error when errors exceed 8%. This number was chosen on the basis of the distance between the opposing sides of the curve in the prints. Data was digitized using [WebPlotdigitizer](https://apps.automeris.io/wpd/).
--- test/constraint_release.cpp
+++ test/constraint_release.cpp
@@ -5,13 +5,88 @@
 
 #define BOOST_TEST_MODULE test_constraint_release
 #include <boost/test/unit_test.hpp>
+#include <boost/test/included/unit_test.hpp>
 
 #include "../src/constraint_release/constraint_release.hpp"
+#include "../src/constraint_release/heuzey.hpp"
 #include "../src/time_series.hpp"
+#include "../src/file_reader.hpp"
+#include "../src/postprocess.hpp"
 
 #include <array>
+#include <filesystem>
+#include <algorithm>
+#include <memory>
 
-BOOST_AUTO_TEST_CASE( constraint_release_factory )
+struct Reproduction_context {
+	using path_impl_pair = std::pair<std::filesystem::path, constraint_release::impl>;
+
+	Reproduction_context()
+	: ctx{std::make_shared<Context>()}
+	{
+		ctx->Z = 300;
+		ctx->tau_e = 1;
+
+		CLF_context_builder builder(ctx);
+    	builder.gather_physics();
+
+    	ctx = builder.get_context();
+    	ctx->apply_physics();
+	}
+
+	~Reproduction_context()
+	{}
+
+	void check_impl(std::filesystem::path path, constraint_release::impl impl)
+	{
+		Time_series input = File_reader::get_file_contents(path, 1);
+
+        auto CR = constraint_release::Factory_with_context::create(impl, 1.0, *ctx);
+
+        auto wrapper = [&CR](const Time_series::value_primitive& t) {return (*CR)(t);};
+
+        Time_series series = dimensionless_derivative(wrapper, input.get_time_range(), ctx);
+
+        auto vals = series.get_values();
+        auto inputs = input.get_values();
+
+        for (size_t i{0} ; i < series.size() ; ++i)
+        {
+            BOOST_TEST_INFO("index: " << i);
+            BOOST_CHECK_CLOSE(vals[i], inputs[i], 8.0);
+        }
+	}
+
+	std::shared_ptr<Context> ctx;
+};
+
+
+BOOST_FIXTURE_TEST_CASE(
+    lm_figure_six,
+    Reproduction_context,
+    * boost::unit_test::label("validation")
+    * boost::unit_test::label("rubinstein")
+    * boost::unit_test::description("Test Rubinstein & Colby method for constraint release against literature data. (doi:10.1063/1.455620)"))
+{
+    check_impl("../validation/likhtmanmcleish/figure6_R.dat", constraint_release::impl::RUBINSTEINCOLBY);
+}
+
+BOOST_FIXTURE_TEST_CASE(
+    heuzey_figure_five,
+    Reproduction_context,
+    * boost::unit_test::label("validation")
+    * boost::unit_test::label("heuzey")
+    * boost::unit_test::description("Test Heuzey method for constraint release against literature data. (doi:10.1002/app.20881)"))
+{
+	check_impl("../validation/heuzey/figure5_R.dat", constraint_release::impl::HEUZEY);
+}
+
+BOOST_AUTO_TEST_CASE(
+    constraint_release_factory,
+    * boost::unit_test::label("factory")
+    * boost::unit_test::label("heuzey")
+    * boost::unit_test::label("rubinstein")
+    * boost::unit_test::description("Test if constraint release factory returns an instance for given entried in constraint_release::impl."))
 {
     std::array<constraint_release::impl, 2> impls{constraint_release::impl::HEUZEY, constraint_release::impl::RUBINSTEINCOLBY};
 
@@ -19,7 +94,8 @@ BOOST_AUTO_TEST_CASE( constraint_release_factory )
 
     for (auto impl : impls)
     {
-        auto CR = constraint_release::Factory::create(impl, time_range, 0.1);
+        BOOST_TEST_INFO(impl);
+        auto CR = constraint_release::Factory::create(impl, 0.1);
         BOOST_CHECK( CR );
     }
 }
